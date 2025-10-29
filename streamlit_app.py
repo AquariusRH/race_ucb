600f2cffbb8fb94f32087a7e2bee911496e7b0fb
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-10-29 19:08:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1080,7 +1080,7 @@ if st.session_state.get('reset', False):
                 df_ucb, top4 = run_ucb_prediction(win_odds, total_win_inv, horses, ucb_state, ucb_state['t'])
 
                 # 開賽前 1 分鐘鎖定
-                if (post_time - time_now).total_seconds() <= 60 and not ucb_state['locked']:
+                if (post_time_dict[race_no] - time_now).total_seconds() <= 60 and not ucb_state['locked']:
                     ucb_state['locked'] = True
                     ucb_state['final_ucb_df'] = df_ucb
 
