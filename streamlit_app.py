95dfbe296e197d6ea828a40396395f05810dbbf1
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-11-04 22:37:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1010,6 +1010,7 @@ top_list = [item for item in top_list_default if item in selected_methods]
 # Define the button callback
 def click_start_button():
     st.session_state.reset = True
+    st.session_state.api_called = False
 
 # --- 開始按鈕 ---
 if st.button("開始", on_click=click_start_button):
